af7c1b305cda6ad97224679f85744f006835c656
D1ZHRNE <rzfoulen@gmail.com>
2018-12-21 22:37:41 -0500

    Change titles and styling on homepage

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
-import new
-
+from flask import Flask, render_template             
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return new
+    return render_template("home.html")
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
 if __name__ == "__main__":
     app.run(debug=True)
